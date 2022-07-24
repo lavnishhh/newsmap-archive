2375246052b93d26d5e4a1a18169e52c2428084d
Lavnish Chaudhary <82657384+lavnishhh@users.noreply.github.com>
2022-07-24 22:28:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -128,14 +128,7 @@ const source_logo_template = `
   <div onclick="updateLinkMenu(this,{onclick-data-ref})" style="background-image: url({image-url});" class="source-logo"></div>
 </div>`
 
-const news_item_template = `<div class="news-item">
-<div>
-    <a href="{news-url}" target="_blank"}>
-      <img target="_blank" src="{image-url}">
-    </a>
-</div>
-<a href="{news-url}" target="_blank">{title}</a>
-</div>`
+const news_item_template = `<div class="news-item"><div><a href="{news-url}" target="_blank"}><img target="_blank" src="{image-url}"></a></div><a href="{news-url}" target="_blank">{title}</a></div>`
 function updateMenu(e){
   if(e.target.metadata){
     updateLinkMenu()
