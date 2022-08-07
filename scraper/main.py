8a08f686d2146722b7ebcfb970e63e377910333c
Lavnish Chaudhary <82657384+lavnishhh@users.noreply.github.com>
2022-08-07 10:08:29 -0700

    Create main.py

---
+++ scraper/main.py
@@ -0,0 +1,36 @@
+print('importing libraries')
+import json
+import extract
+import time as t
+print('imported libraries')
+
+source_in = ['ndtv','ht','inexp','rw','abp','idto','news18','et','zee','timnow']
+#source_in = ['inexp']
+#rw time
+with open('data/data.json', 'r') as j:
+     json_inp = json.loads(j.read())
+
+def update_data(sources):
+    source_index = {}
+    ind = 0
+    for source in json_inp:
+        source_index[source['source_tag']] = ind
+        ind+=1
+    for source_id in sources:
+        so_t = t.time()
+        print('started '+source_id)
+        if(source_id in source_index):
+            json_inp[source_index[source_id]] = getattr(extract, source_id)()
+        else:
+            json_inp.append(getattr(extract, source_id)())
+        print('finished '+source_id)
+        print(t.time() - so_t)
+
+        with open('data/data.json', 'w') as jso:
+                json.dump(json_inp,jso,indent=3)
+
+ti = t.time()
+
+update_data(source_in)
+
+print(t.time()-ti)
